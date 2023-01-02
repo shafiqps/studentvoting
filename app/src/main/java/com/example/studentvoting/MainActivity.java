fbbf14120d0656df38b28b1c4b0af2a3d60f1bb9
Conveur <amik.sepam@yahoo.com>
2023-01-02 18:01:23 +0800

    Connected each button on the bottom navigation menu to the respective pages/fragments.

--- app/src/main/java/com/example/studentvoting/MainActivity.java
+++ app/src/main/java/com/example/studentvoting/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.studentvoting;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -23,54 +26,39 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+    BottomNavigationView bottomNavigationView;
+    Home homeFragment = new Home();
+    ElectionPage electionPageFragment = new ElectionPage();
+    ProfilePage profilePageFragment = new ProfilePage();
+    SettingsPage settingsPageFragment = new SettingsPage();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.Home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.electionPage:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, electionPageFragment).commit();
+                        return true;
+                    case R.id.profilePage:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, profilePageFragment).commit();
+                        return true;
+                    case R.id.settingsPage:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, settingsPageFragment).commit();
+                        return true;
+                }
+                return false;
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
