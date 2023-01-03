8a89ccba4d13ce21f375de2bf619ba81e23acfbb
Conveur <amik.sepam@yahoo.com>
2023-01-04 02:12:30 +0800

    Added tab selection in Result page.

--- app/src/main/java/com/example/studentvoting/MainActivity.java
+++ app/src/main/java/com/example/studentvoting/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
