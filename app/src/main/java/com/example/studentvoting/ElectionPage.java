2d96091a9682a44a58f86e1132063904dff181ee
shazwydaniel <shazwydaniel@icloud.com>
2023-01-05 17:10:13 +0800

    election page update

--- app/src/main/java/com/example/studentvoting/ElectionPage.java
+++ app/src/main/java/com/example/studentvoting/ElectionPage.java
@@ -7,11 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ElectionPage extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_election_page, container, false);
+
     }
 }
