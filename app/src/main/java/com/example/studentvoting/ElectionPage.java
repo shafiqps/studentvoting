5b85b0edfc1ed1e7d55a03c70238525a251d1d19
Daus1256 <u2001256@siswa.um.edu.my>
2023-01-05 18:38:12 +0800

    casting vote fragment dh sambung dgn election page

--- app/src/main/java/com/example/studentvoting/ElectionPage.java
+++ app/src/main/java/com/example/studentvoting/ElectionPage.java
@@ -3,35 +3,38 @@ package com.example.studentvoting;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class ElectionPage extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_election_page, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_election_page, container, false);
 
+        Button BtnToCastVote = (Button) rootView.findViewById(R.id.goToVotingButton);
+
+        BtnToCastVote.setOnClickListener(this::onClick);
+        return rootView;
     }
-/*
-    public void importantdates(View view) {
+
+    private void onClick(View view) {
         Fragment fragment = null;
         switch (view.getId()) {
-            case R.id.textView16:
-                fragment = new Result();
+            case R.id.goToVotingButton:
+                fragment = new VoteCast();
                 replaceFragment(fragment);
                 break;
         }
     }
-
     public void replaceFragment(Fragment someFragment) {
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
         transaction.replace(R.id.container, someFragment);
         transaction.addToBackStack(null);
         transaction.commit();
-    } */
+    }
 }
