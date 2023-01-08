71e099493b329ef9828907353fe4dd269c798b37
shafiqps <shafiqaimanps@gmail.com>
2023-01-09 01:40:05 +0800

    verify vote comment babi cibai

--- app/src/main/java/com/example/studentvoting/VerifyVote.java
+++ app/src/main/java/com/example/studentvoting/VerifyVote.java
@@ -1,5 +1,9 @@
 package com.example.studentvoting;
 
+import static android.hardware.biometrics.BiometricManager.Authenticators.BIOMETRIC_STRONG;
+import static android.hardware.biometrics.BiometricManager.Authenticators.DEVICE_CREDENTIAL;
+
+import android.content.Intent;
 import android.hardware.biometrics.BiometricManager;
 import android.hardware.biometrics.BiometricPrompt;
 import android.os.Build;
@@ -10,6 +14,8 @@ import androidx.annotation.RequiresApi;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,9 +29,9 @@ import java.util.concurrent.Executor;
 
 public class VerifyVote extends Fragment {
 
-    BiometricPrompt biometricPrompt;
-    BiometricPrompt.PromptInfo promptInfo;
-    RelativeLayout verification_layout;
+//    BiometricPrompt biometricPrompt;
+//    BiometricPrompt.PromptInfo promptInfo;
+//    RelativeLayout verification_layout;
 
 
 
@@ -33,55 +39,63 @@ public class VerifyVote extends Fragment {
         // Required empty public constructor
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.Q)
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_verify_vote, container, false);
+//    @RequiresApi(api = Build.VERSION_CODES.Q)
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+////                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        View view = inflater.inflate(R.layout.fragment_verify_vote, container, false);
+//
+//
+//        verification_layout = view.findViewById(R.id.verification_fragment);
+//
+//        BiometricManager biometricManager = BiometricManager.from(this);
+//        switch (biometricManager.canAuthenticate(BIOMETRIC_STRONG | DEVICE_CREDENTIAL)) {
+//            case BiometricManager.BIOMETRIC_SUCCESS:
+//                Log.d("MY_APP_TAG", "App can authenticate using biometrics.");
+//                break;
+//            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+//                Log.e("MY_APP_TAG", "No biometric features available on this device.");
+//                break;
+//            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+//                Log.e("MY_APP_TAG", "Biometric features are currently unavailable.");
+//                break;
+//            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+//                // Prompts the user to create credentials that your app accepts.
+//                final Intent enrollIntent = new Intent(Settings.ACTION_BIOMETRIC_ENROLL);
+//                enrollIntent.putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+//                        BIOMETRIC_STRONG | DEVICE_CREDENTIAL);
+//                startActivityForResult(enrollIntent, REQUEST_CODE);
+//                break;
+//        }
+//
+//        Executor executor = ContextCompat.getMainExecutor(view.getContext());
+//
+//        biometricPrompt = new androidx.biometric.BiometricPrompt(verification_layout.this, executor, new androidx.biometric.BiometricPrompt.AuthenticationCallback() {
+//            @Override
+//            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+//                super.onAuthenticationError(errorCode, errString);
+//            }
+//
+//            @Override
+//            public void onAuthenticationSucceeded(@NonNull androidx.biometric.BiometricPrompt.AuthenticationResult result) {
+//                super.onAuthenticationSucceeded(result);
+//                Toast.makeText(getActivity().getApplicationContext(), "Your Vote is verified", Toast.LENGTH_SHORT).show();
+//                verification_layout.setVisibility(View.VISIBLE);
+//            }
+//
+//            @Override
+//            public void onAuthenticationFailed() {
+//                super.onAuthenticationFailed();
+//            }
+//        });
+//
+//        promptInfo = new BiometricPrompt.PromtInfo.Builder().setTitle("Verified Your Vote with Your FingerPrint")
+//                .setDescription("Use Fingerprint to Verify Your Vote").setDeviceCredentialAllowed(true).build();
+//
+//        biometricPrompt.authenticate(promptInfo);
 
 
-        verification_layout = view.findViewById(R.id.verification_fragment);
+//        return view;
 
-        BiometricManager biometricManager = (BiometricManager) getActivity().getSystemService(VerifyVote.class);
-        switch(biometricManager.canAuthenticate()){
-            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
-                Toast.makeText(getActivity().getApplicationContext(), "Device don't have FingerPrint feature", Toast.LENGTH_SHORT).show();
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
-                Toast.makeText(getActivity().getApplicationContext(), "FingerPrint doesn't working", Toast.LENGTH_SHORT).show();
-            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
-                Toast.makeText(getActivity().getApplicationContext(), "No FingerPrint assigned", Toast.LENGTH_SHORT).show();
-
-        }
-
-        Executor executor = ContextCompat.getMainExecutor(view.getContext());
-
-        biometricPrompt = new androidx.biometric.BiometricPrompt(verification_layout.this, executor, new androidx.biometric.BiometricPrompt.AuthenticationCallback() {
-            @Override
-            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
-                super.onAuthenticationError(errorCode, errString);
-            }
-
-            @Override
-            public void onAuthenticationSucceeded(@NonNull androidx.biometric.BiometricPrompt.AuthenticationResult result) {
-                super.onAuthenticationSucceeded(result);
-                Toast.makeText(getActivity().getApplicationContext(), "Your Vote is verified", Toast.LENGTH_SHORT).show();
-                verification_layout.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void onAuthenticationFailed() {
-                super.onAuthenticationFailed();
-            }
-        });
-
-        promptInfo = new BiometricPrompt.PromtInfo.Builder().setTitle("Verified Your Vote with Your FingerPrint")
-                .setDescription("Use Fingerprint to Verify Your Vote").setDeviceCredentialAllowed(true).build();
-
-        biometricPrompt.authenticate(promptInfo);
-
-
-        return view;
-    }
 }
