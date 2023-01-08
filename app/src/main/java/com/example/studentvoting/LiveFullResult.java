ee1282af9cd818de54af175671e144968d1ac172
Conveur <amik.sepam@yahoo.com>
2023-01-09 02:53:08 +0800

    Changes in live full result layout

--- app/src/main/java/com/example/studentvoting/LiveFullResult.java
+++ app/src/main/java/com/example/studentvoting/LiveFullResult.java
@@ -42,12 +42,53 @@ public class LiveFullResult extends Fragment {
     private List<Faculty> facultyList(){
         List<Faculty> facultyList = new ArrayList<>();
 
-        Faculty faculty = new Faculty("FSKTM", candidateList());
-        facultyList.add(faculty);
-
-        Faculty faculty1 = new Faculty("FLL", candidateList());
+        Faculty faculty1 = new Faculty("Academy of Islamic Studies", candidateList());
         facultyList.add(faculty1);
 
+        Faculty faculty2 = new Faculty("Academy of Malay Studies", candidateList());
+        facultyList.add(faculty2);
+
+        Faculty faculty3 = new Faculty("Faculty of Arts and Social Sciences", candidateList());
+        facultyList.add(faculty3);
+
+        Faculty faculty4 = new Faculty("Faculty of Built Environment", candidateList());
+        facultyList.add(faculty4);
+
+        Faculty faculty5 = new Faculty("Faculty of Business and Accountancy", candidateList());
+        facultyList.add(faculty5);
+
+        Faculty faculty6 = new Faculty("Faculty of Computer Science and Information Technology", candidateList());
+        facultyList.add(faculty6);
+
+        Faculty faculty7 = new Faculty("Faculty of Creative Arts", candidateList());
+        facultyList.add(faculty7);
+
+        Faculty faculty8 = new Faculty("Faculty of Dentistry", candidateList());
+        facultyList.add(faculty8);
+
+        Faculty faculty9 = new Faculty("Faculty of Economics and Administration", candidateList());
+        facultyList.add(faculty9);
+
+        Faculty faculty10 = new Faculty("Faculty of Education", candidateList());
+        facultyList.add(faculty10);
+
+        Faculty faculty11 = new Faculty("Faculty of Engineering", candidateList());
+        facultyList.add(faculty11);
+
+        Faculty faculty12 = new Faculty("Faculty of Languages and Linguistics", candidateList());
+        facultyList.add(faculty12);
+
+        Faculty faculty13 = new Faculty("Faculty of Law", candidateList());
+        facultyList.add(faculty13);
+
+        Faculty faculty14 = new Faculty("Faculty of Medicine", candidateList());
+        facultyList.add(faculty14);
+
+        Faculty faculty15 = new Faculty("Faculty of Science", candidateList());
+        facultyList.add(faculty15);
+
+        Faculty faculty16 = new Faculty("Faculty of Sports and Exercise Science", candidateList());
+        facultyList.add(faculty16);
         return facultyList;
     }
 
