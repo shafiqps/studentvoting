bad3cbadad3e5c68b0699e58c79c7b6f34e760af
Ilham <ilham130602@gmail.com>
2023-01-09 17:47:57 +0800

    scrollable recyclerview

--- app/src/main/java/com/example/studentvoting/ProfilePage.java
+++ app/src/main/java/com/example/studentvoting/ProfilePage.java
@@ -3,15 +3,39 @@ package com.example.studentvoting;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ProfilePage extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile_page, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_profile_page, container, false);
+        TextView facTV = (TextView) rootview.findViewById(R.id.facultyTV);
+        facTV.setOnClickListener(this::onClick);
+        return rootview;
     }
+
+    private void onClick(View view) {
+        Fragment fragment = null;
+        switch (view.getId()) {
+            case R.id.facultyTV:
+                fragment = new FacultyProfile();
+                replaceFragment(fragment);
+                break;
+        }
+    }
+
+
+    public void replaceFragment(Fragment someFragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, someFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
 }
