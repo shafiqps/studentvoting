b8f7ca6caef1e9ce2087cac8abfec246f758daf5
shafiqps <shafiqaimanps@gmail.com>
2023-01-11 01:24:40 +0800

    vote cast

--- app/src/main/java/com/example/studentvoting/VoteCast.java
+++ app/src/main/java/com/example/studentvoting/VoteCast.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.hardware.biometrics.BiometricPrompt;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -19,9 +20,15 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class VoteCast extends Fragment {
-
+    DatabaseReference reff;
     //candidate selection using radio button
     RadioGroup radioGroup;
     RadioButton radioButton;
@@ -32,8 +39,46 @@ public class VoteCast extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String facultyID = MainActivity.currentfacultyPage;
         // Inflate the layout for this fragment
+        reff = FirebaseDatabase.getInstance("https://studentvoting-fc2ca-default-rtdb.asia-southeast1.firebasedatabase.app").getReference();
+
         View rootView = inflater.inflate(R.layout.fragment_vote_cast, container, false);
+        TextView facultyName = rootView.findViewById(R.id.textFacultyName);
+        TextView candidateA = rootView.findViewById(R.id.candidateNameA);
+        TextView partyA = rootView.findViewById(R.id.candidatePartyA);
+        TextView candidateB = rootView.findViewById(R.id.candidateNameB);
+        TextView partyB = rootView.findViewById(R.id.candidatePartyB);
+        TextView candidateC = rootView.findViewById(R.id.candidateNameC);
+        TextView partyC = rootView.findViewById(R.id.candidatePartyC);
+
+        facultyName.setText(facultyID);
+        reff.child("Faculty/"+facultyID+"/candidates").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                final String nameA = snapshot.child("1").child("name").getValue(String.class);
+                final String partA = snapshot.child("1").child("party").getValue(String.class);
+                final String nameB = snapshot.child("2").child("name").getValue(String.class);
+                final String partB = snapshot.child("2").child("party").getValue(String.class);
+                final String nameC = snapshot.child("3").child("name").getValue(String.class);
+                final String partC = snapshot.child("3").child("party").getValue(String.class);
+
+
+                candidateA.setText(nameA);
+                partyA.setText(partA);
+                candidateB.setText(nameB);
+                partyB.setText(partB);
+                candidateC.setText(nameC);
+                partyC.setText(partC);
+//                Picasso.get().load(link).into(rImage);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         context = getActivity();
         //candidate selection using radio button
